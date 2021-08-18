de216669a8a48380765a0f52c0a49d33118b5de5
Terry Phelps <tgphelps50@gmail.com>
2021-08-18 14:45:17 -0400

    Initial commit.

---
+++ bj-trainer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import tkinter as tk
+# from tkinter import ttk
+
+
+class GameWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == '__main__':
+    gw = GameWindow()
+    gw.mainloop()
